15f75af51f95e0ba60b3bfe9940cf78ea91e4883
ornob011 <ornob011@gmail.com>
2026-01-13 20:40:38 +0600

    1-add-dependencies: Add main java file

---
+++ src/main/java/com/dsi/support/agenticrouter/AgenticTicketRouterApplication.java
@@ -0,0 +1,13 @@
+package com.dsi.support.agenticrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenticTicketRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenticTicketRouterApplication.class, args);
+	}
+
+}
