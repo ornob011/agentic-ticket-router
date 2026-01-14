f3bd84b7c06ef11b33623f33304ce5f2db85b645
ornob011 <ornob011@gmail.com>
2026-01-14 14:20:10 +0600

    8-signup: Add utility class to hold constants

---
+++ src/main/java/com/dsi/support/agenticrouter/util/Constants.java
@@ -0,0 +1,15 @@
+package com.dsi.support.agenticrouter.util;
+
+public class Constants {
+    private Constants() {
+        throw new UnsupportedOperationException("Utility class cannot be instantiated");
+    }
+
+    public static final String VIEW_PREFIX = "views/";
+    public static final String HOME_URL = "/";
+    public static final String LOGIN_URL = "/login";
+    public static final String FLUSH_SUCCESS_MSG_CODE = "flushSuccessMessage";
+    public static final String FLUSH_ERROR_MSG_CODE = "flushErrorMessage";
+    public static final String FLUSH_WARNING_MSG_CODE = "flushWarningMessage";
+    public static final String FLUSH_INFO_MSG_CODE = "flushInfoMessage";
+}
