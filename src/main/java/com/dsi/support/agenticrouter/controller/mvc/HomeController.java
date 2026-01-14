5876c35fb2fb2f7a0fcffb4035f01031a207c04d
ornob011 <ornob011@gmail.com>
2026-01-14 14:40:22 +0600

    8-signup: Add home controller

---
+++ src/main/java/com/dsi/support/agenticrouter/controller/mvc/HomeController.java
@@ -0,0 +1,22 @@
+package com.dsi.support.agenticrouter.controller.mvc;
+
+import com.dsi.support.agenticrouter.util.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping(value = "/")
+    public String home() {
+        if (!Utils.isLoggedIn()) {
+            return "redirect:/login";
+        }
+
+        return "redirect:/dashboard";
+    }
+
+}
