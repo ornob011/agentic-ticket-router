d8c28dc8a89cfa9581bdcf31582cd27cd50397ac
ornob011 <ornob011@gmail.com>
2026-02-16 02:16:41 +0600

    39-human-review: Fix typo in CategoryDetectionListener

--- src/main/java/com/dsi/support/agenticrouter/listener/CategoryDetectionListener.java
+++ src/main/java/com/dsi/support/agenticrouter/listener/CategoryDetectionListener.java
@@ -20,7 +20,7 @@ public class CategoryDetectionListener {
         CategoryDetectionEvent categoryDetectionEvent
     ) {
         categoryDetectionEventHandlerService.handleCategoryDetection(
-
+            categoryDetectionEvent
         );
     }
 }
