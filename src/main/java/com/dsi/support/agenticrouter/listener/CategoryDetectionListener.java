4df00c722e53ce158ee1008d396ac8e6cdb6b512
ornob011 <ornob011@gmail.com>
2026-02-16 10:39:46 +0600

    39-human-review: Add log statements into CategoryDetectionListener

--- src/main/java/com/dsi/support/agenticrouter/listener/CategoryDetectionListener.java
+++ src/main/java/com/dsi/support/agenticrouter/listener/CategoryDetectionListener.java
@@ -2,7 +2,10 @@ package com.dsi.support.agenticrouter.listener;
 
 import com.dsi.support.agenticrouter.event.CategoryDetectionEvent;
 import com.dsi.support.agenticrouter.service.ticket.CategoryDetectionEventHandlerService;
+import com.dsi.support.agenticrouter.util.OperationalLogContext;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -10,6 +13,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CategoryDetectionListener {
 
     private final CategoryDetectionEventHandlerService categoryDetectionEventHandlerService;
@@ -19,8 +23,23 @@ public class CategoryDetectionListener {
     public void handleCategoryDetection(
         CategoryDetectionEvent categoryDetectionEvent
     ) {
+        log.info(
+            "CategoryDetectionEventHandle({}) SupportTicket(id:{}) Actor(id:{}) Outcome(replyLength:{})",
+            OperationalLogContext.PHASE_START,
+            categoryDetectionEvent.getTicketId(),
+            categoryDetectionEvent.getCustomerId(),
+            StringUtils.length(categoryDetectionEvent.getReplyContent())
+        );
+
         categoryDetectionEventHandlerService.handleCategoryDetection(
             categoryDetectionEvent
+        );
+
+        log.info(
+            "CategoryDetectionEventHandle({}) SupportTicket(id:{}) Outcome(handler:{})",
+            OperationalLogContext.PHASE_COMPLETE,
+            categoryDetectionEvent.getTicketId(),
+            "executed"
         );
     }
 }
