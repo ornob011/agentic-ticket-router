497e47f32dde7878803ccc4a7bc00c62d4d09018
ornob011 <ornob011@gmail.com>
2026-02-17 11:42:30 +0600

    41-frontend-design-pattern: Add eslint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,37 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default tseslint.config(
+  { ignores: ["dist", "node_modules", "vite.config.ts", "tailwind.config.ts"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json", "./tsconfig.node.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+      "@typescript-eslint/prefer-promise-reject-errors": "off",
+      "@typescript-eslint/only-throw-error": "off",
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  }
+);
