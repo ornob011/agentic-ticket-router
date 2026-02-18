629461cd636a7038b0cc529ca62f23949e3f4337
ornob011 <ornob011@gmail.com>
2026-02-18 13:29:16 +0600

    43-agentic-action: Add new enums for agentic workflow

---
+++ src/main/java/com/dsi/support/agenticrouter/enums/AgentRuntimeRunStatus.java
@@ -0,0 +1,7 @@
+package com.dsi.support.agenticrouter.enums;
+
+public enum AgentRuntimeRunStatus {
+    RUNNING,
+    COMPLETED,
+    FAILED
+}
---
+++ src/main/java/com/dsi/support/agenticrouter/enums/AgentTerminationReason.java
@@ -0,0 +1,10 @@
+package com.dsi.support.agenticrouter.enums;
+
+public enum AgentTerminationReason {
+    GOAL_REACHED,
+    SAFETY_BLOCKED,
+    BUDGET_EXCEEDED,
+    RUNTIME_TIMEOUT,
+    PLAN_VALIDATION_FAILED,
+    TOOL_FAILURE
+}
